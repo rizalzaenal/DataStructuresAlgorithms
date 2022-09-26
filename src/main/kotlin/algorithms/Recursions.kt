208bd355fa5213251078cc5530b47107ba59c7d9
rizal <rizaenalofficial@gmail.com>
2022-09-26 09:14:53 +0700

    add Recursions practice

--- src/main/kotlin/algorithms/Recursions.kt
+++ src/main/kotlin/algorithms/Recursions.kt
@@ -26,4 +26,33 @@ object Recursions {
 
     }
 
+    fun stringReversal(input: String): String {
+        if (input.length == 1) return input[0].toString()
+
+        return  stringReversal(input.substring(1)) + input[0]
+    }
+
+    fun isPalindrome(input: String): Boolean {
+        if (input.length == 0 || input.length == 1) return true
+
+        if (input[0] == input[input.length - 1]) {
+            return isPalindrome(input.substring(1, input.length - 1))
+        }
+
+        return false
+    }
+
+    fun decimalToBinary(decimal: Int, result: String = ""): String {
+        if (decimal == 0) return result
+
+        val binary = (decimal % 2).toString() + result
+
+        return decimalToBinary(decimal / 2, binary)
+    }
+
+    fun sumNaturalNumbers(number: Int): Int {
+        if (number == 1) return 1
+
+        return sumNaturalNumbers(number - 1) + number
+    }
 }
